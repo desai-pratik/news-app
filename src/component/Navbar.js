2ad9fc2da0e5c8155df8986cbfc0f209a24cc32f
desai-pratik <pratikdesai2274@gmail.com>
2023-10-11 03:36:09 +0530

    add console remove

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -25,7 +25,7 @@ export class Navbar extends Component {
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                 </button>
-                <div className="collapse navbar-collapse" id="navbarScroll">
+                <div className="collapse navbar-collapse" id="navbarScroll"> 
                 {/* <ul className="navbar-nav me-auto my-2 my-lg-0 navbar-nav-scroll text-center">
                    <li className="nav-item"><Link className="nav-link" aria-current="page" to="/">Home</Link></li>
                    <li className="nav-item"><Link className="nav-link" to='/business'>Business</Link></li>
